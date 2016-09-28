94607334caacae65d53da19346f797b602869573
dstaltman <dstaltman@gmail.com>
2016-09-27 23:51:27 -0700

    Add express server and move some files around

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,26 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  entry: './main.js',
-  output: { path: __dirname, filename: 'bundle.js' },
+  entry: './src/main.js',
+  output: {
+    path: './public',
+    filename: 'bundle.js'
+  },
   module: {
     loaders: [
       {
+        test: /.js?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
         test: /.jsx?$/,
         loader: 'babel-loader',
-        exclude: /node_modules/,
-        query: {
-          presets: ['es2015', 'react']
-        }
+        exclude: /node_modules/
       }
     ]
   },
+  resolve: {
+    extensions: ['', '.js', '.json']
+  }
 };
